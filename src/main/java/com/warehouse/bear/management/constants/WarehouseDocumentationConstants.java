e67ac78d5a0f3e0938513ddc8e80ac304826ec59
Randrin Nzeukang <nrandrin@eservicios.indracompany.com>
2022-05-10 13:21:33 +0200

    disabled/activate user api

--- src/main/java/com/warehouse/bear/management/constants/WarehouseDocumentationConstants.java
+++ src/main/java/com/warehouse/bear/management/constants/WarehouseDocumentationConstants.java
@@ -19,4 +19,5 @@ public class WarehouseDocumentationConstants {
     public static final String WAREHOUSE_OPERATION_VERIFY_LINK = "Verify the user link provided with the verify type";
     public static final String WAREHOUSE_OPERATION_RESET_PASSWORD = "Reset your password to warehouse system";
     public static final String WAREHOUSE_OPERATION_CHANGE_PASSWORD = "Change your password to warehouse system";
+    public static final String WAREHOUSE_OPERATION_CHANGE_STATUS = "Activate or disable user in warehouse system";
 }
